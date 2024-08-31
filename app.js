5cac49d589a7ead8e399ffa60197d44ac96f66d1
minhdat6312 <minhdat6321@gmail.com>
2024-08-31 00:12:51 -0700

    change something in app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-require(
-  "dotenv"
-).config()
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const indexRouter = require("./routes/index");
 
-var indexRouter = require('./routes/index');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
 
 
 app.use('/', indexRouter);
